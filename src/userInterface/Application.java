909984d8406ec90028e872413bf45607222b31fa
Ponnulakshmi <sponnulakshmi@yahoo.com>
2013-10-27 12:57:26 +0100

    some more modifications to Application

--- src/userInterface/Application.java
+++ src/userInterface/Application.java
@@ -19,9 +19,6 @@ import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
 public class Application extends JFrame implements KeyListener {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel topPanel;
 	private JTextPane tPane;
@@ -33,7 +30,6 @@ public class Application extends JFrame implements KeyListener {
 		setSize(400, 500);
 
 		EmptyBorder eb = new EmptyBorder(new Insets(0, 0, 0, 0));
-
 		tPane = new JTextPane();
 		topPanel.setBorder(eb);
 		tPane.setBorder(eb);
@@ -55,8 +51,6 @@ public class Application extends JFrame implements KeyListener {
 				"Lucida Console");
 		aset = sc.addAttribute(aset, StyleConstants.Alignment,
 				StyleConstants.ALIGN_LEFT);
-		// tp.setParagraphAttributes(aset, true);
-		// tp.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
 		int len = tp.getDocument().getLength();
 		tp.setCaretPosition(len);
@@ -73,36 +67,55 @@ public class Application extends JFrame implements KeyListener {
 				try {
 					new Application();
 				} catch (AWTException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		});
 	}
 
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-			e.consume();
-		}
-
-	}
-
 	@Override
 	public void keyReleased(KeyEvent e) {
+		String text = "";
 		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			text = tPane.getText().substring(
+					tPane.getText().lastIndexOf(">") + 1,
+					tPane.getText().length());
+			System.out.print("tPane : " + tPane.getDocument().getLength()
+					+ "  text: " + text);
+			if (text.contains("connect") || text.contains("send")
+					|| text.contains("help") || text.equals("")) {
+
+			} else {
+				appendToPane(tPane, "Please enter a valid command. \n",
+						Color.RED);
+				e.setKeyCode(KeyEvent.VK_ENTER);
+			}
+			tPane.setEditable(true);
 			appendToPane(tPane, "EchoClient>", Color.RED);
 			appendToPane(tPane, "", Color.BLACK);
 		}
-		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+		AttributeSet attributeSet = tPane.getInputAttributes();
+		Color c = (Color) (attributeSet == null ? null : attributeSet
+				.getAttribute(StyleConstants.Foreground));
+		System.out.println("Color : " + c.getRed());
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE && c.getRed() == 255) {
+			appendToPane(tPane, "", Color.BLACK);
 			e.consume();
+		}
+		if (c.getRed() == 255) {
+			tPane.setEditable(false);
+			appendToPane(tPane, "", Color.BLACK);
+			tPane.setEditable(true);
 		}
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-			e.consume();
-		}
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 	}
 
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
 }
