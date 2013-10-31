284ca25e593624c19be2ff7d18f407cd32b38271
Ponnulakshmi <sponnulakshmi@yahoo.com>
2013-10-31 10:32:33 +0100

    byte array conversion not working. need to improvise

--- src/clientPrgm/EchoServerByte.java
+++ src/clientPrgm/EchoServerByte.java
@@ -6,6 +6,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServerByte {
+	public char one;
+	public char[] mess = new char[128 * 8];
+	public byte[] msg = new byte[128 * 8];
+	public int i = 0;
 
 	public EchoServerByte(int portnum) {
 		try {
@@ -23,11 +27,17 @@ public class EchoServerByte {
 						client.getInputStream());
 				DataOutputStream out = new DataOutputStream(
 						client.getOutputStream());
-				out.writeBytes("Welcome to the Java EchoServer.  Type 'bye' to close.");
+				System.out
+						.println("Welcome to the Java EchoServer.  Type 'bye' to close.");
+				out.writeBytes("Welcome to the Java EchoServer.  Type 'bye' to close. \n");
+
 				String line;
 				byte[] msg = new byte[128 * 8];
 				do {
-					in.readFully(msg);
+					while (((one = in.readChar()) != '\n') && i < (128 * 8)) {
+						mess[i] = one;
+					}
+					msg = mess.toString().getBytes();
 					line = new String(msg);
 					if (line != null)
 						out.writeBytes("Got: " + line);
--- src/userInterface/Application.java
+++ src/userInterface/Application.java
@@ -5,10 +5,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 import javax.swing.JFrame;
@@ -20,40 +19,46 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public class Application extends JFrame implements KeyListener {
+
 	private static final long serialVersionUID = 1L;
 	private JPanel topPanel;
 	private JTextPane tPane;
 	private String text;
 	String line;
 	public Socket s;
-	public BufferedReader r;
-	public PrintWriter w;
+	public DataInputStream in;
+	public DataOutputStream out;
+	byte one;
+	char[] mess = new char[128 * 8];
+	byte[] msg = new byte[128 * 8];
+	int i = 0;
+	private static Logger mylogger = Logger.getLogger(Application.class);
+
 	public String[] host = new String[2];
 
 	public Application() throws AWTException {
+
 		topPanel = new JPanel();
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 500);
 
-		// EmptyBorder eb = new EmptyBorder(new Insets(0, 0, 0, 0));
 		tPane = new JTextPane();
-		// topPanel.setBorder(eb);
-		// tPane.setBorder(eb);
-		// topPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-		// tPane.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-		// tPane.setMargin(new Insets(5, 5, 5, 5));
 		setBackground(Color.WHITE);
 		topPanel.setBackground(Color.WHITE);
 		tPane.setBackground(Color.WHITE);
 		appendToPane(tPane, "EchoClient>", Color.DARK_GRAY);
 		appendToPane(tPane, "", Color.BLACK);
 		getContentPane().add(topPanel, BorderLayout.WEST);
-		// pack();
 		tPane.addKeyListener(this);
 		topPanel.add(tPane, BorderLayout.WEST);
 		setVisible(true);
+
 	}
 
 	private void appendToPane(JTextPane tp, String msg, Color c) {
@@ -78,31 +83,67 @@ public class Application extends JFrame implements KeyListener {
 			host[1] = text.split("\\s+")[2];
 			try {
 				s = new Socket(host[0], Integer.parseInt(host[1]));
-				r = new BufferedReader(
-						new InputStreamReader(s.getInputStream()));
-				w = new PrintWriter(s.getOutputStream(), true);
-				line = r.readLine();
-				if (line != null) {
-					appendToPane(tPane, line + "\n", Color.BLACK);
+				in = new DataInputStream(s.getInputStream());
+				out = new DataOutputStream(s.getOutputStream());
+				System.out.println("before while");
+				while (((one = in.readByte()) != '\n') && i < (128 * 8)) {
+					msg[i] = one;
+				}
+				System.out.println(msg.toString());
+				if (msg != null) {
+					appendToPane(tPane, msg.toString() + "\n", Color.BLACK);
 					appendToPane(tPane, "EchoClient>", Color.DARK_GRAY);
 				}
 			} catch (Exception e1) {
 				appendToPane(tPane, e1.getMessage() + "\n", Color.RED);
 				e1.printStackTrace();
+
 			}
 
 		} else if (text.contains("send")) {
 			try {
 				text = text.replace("send", "").trim();
-				w.println(text);
-				line = r.readLine();
-				appendToPane(tPane, line + "\n", Color.BLACK);
-				appendToPane(tPane, "EchoClient>", Color.DARK_GRAY);
+				out.writeBytes(text);
+				while (((one = in.readByte()) != '\n') && i < (128 * 8)) {
+					msg[i] = one;
+				}
+				if (msg != null) {
+					appendToPane(tPane, new String(msg) + "\n", Color.BLACK);
+					appendToPane(tPane, "EchoClient>", Color.DARK_GRAY);
+				}
 			} catch (Exception e1) {
 				appendToPane(tPane, e1.getMessage() + "\n", Color.RED);
 				e1.printStackTrace();
+				mylogger.debug("HELLLO");
 			}
 
+		}
+
+		else if (text.contains("logLevel")) {
+			try {
+				text = text.replace("logLevel", "").trim();
+				String s = text.toUpperCase();
+				if (s.equals("ALL"))
+					mylogger.setLevel(Level.ALL);
+				if (s.equals("DEBUG"))
+					mylogger.setLevel(Level.DEBUG);
+				if (s.equals("INFO"))
+					mylogger.setLevel(Level.INFO);
+				if (s.equals("WARN"))
+					mylogger.setLevel(Level.WARN);
+				if (s.equals("ERROR"))
+					mylogger.setLevel(Level.ERROR);
+				if (s.equals("FATAL"))
+					mylogger.setLevel(Level.FATAL);
+				if (s.equals("OFF"))
+					mylogger.setLevel(Level.OFF);
+				if (s.equals("TRACE"))
+					mylogger.setLevel(Level.TRACE);
+				out.writeBytes(text);
+
+			} catch (Exception e) {
+
+			}
 		} else if (text.trim().equals("disconnect")) {
 			try {
 				s.close();
@@ -121,24 +162,30 @@ public class Application extends JFrame implements KeyListener {
 			dispose();
 		} else if (text.trim().equals("help")) {
 
-			appendToPane(tPane, "Type the following comments \n "
-					+ "connect <Server> <Port> - to connect to the server \n "
-					+ "send <message> - to send message to the server \n "
-					+ "disconnect - to disconnect from the server \n "
-					+ "quit - to quit the application \n "
-					+ "log <level> - to set the log level for the logger. \n",
+			appendToPane(
+					tPane,
+					"Type the following comments \n "
+							+ "connect <Server> <Port> - to connect to the server \n "
+							+ "send <message> - to send message to the server \n "
+							+ "disconnect - to disconnect from the server \n "
+							+ "quit - to quit the application \n "
+							+ "logLevel <level> - to set the log level for the logger. \n",
 					Color.RED);
 			appendToPane(tPane, "EchoClient>", Color.DARK_GRAY);
+
 		} else {
-			appendToPane(tPane, "Please enter a valid command. \n "
-					+ "Type the following comments \n "
-					+ "connect <Server> <Port> - to connect to the server \n "
-					+ "send <message> - to send message to the server \n "
-					+ "disconnect - to disconnect from the server \n "
-					+ "quit - to quit the application \n "
-					+ "log <level> - to set the log level for the logger. \n",
+			appendToPane(
+					tPane,
+					"Please enter a valid command. \n "
+							+ "Type the following comments \n "
+							+ "connect <Server> <Port> - to connect to the server \n "
+							+ "send <message> - to send message to the server \n "
+							+ "disconnect - to disconnect from the server \n "
+							+ "quit - to quit the application \n "
+							+ "logLevel <level> - to set the log level for the logger. \n",
 					Color.RED);
 			appendToPane(tPane, "EchoClient>", Color.DARK_GRAY);
+
 		}
 	}
 
@@ -159,6 +206,7 @@ public class Application extends JFrame implements KeyListener {
 				e1.printStackTrace();
 			}
 			tPane.setEditable(true);
+
 			appendToPane(tPane, "", Color.BLACK);
 		}
 		AttributeSet attributeSet = tPane.getInputAttributes();
@@ -181,6 +229,7 @@ public class Application extends JFrame implements KeyListener {
 
 	public String getText() {
 		return text;
+
 	}
 
 	@Override
@@ -194,12 +243,25 @@ public class Application extends JFrame implements KeyListener {
 	}
 
 	public static void main(String[] args) {
+
+		// /PropertyConfigurator.configure("log4j.properties");
+		org.apache.log4j.BasicConfigurator.configure();
+		// PropertyConfigurator.configure("log.config");
+
+		mylogger.info("Log msg");
+
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					new Application();
 				} catch (AWTException e) {
 					e.printStackTrace();
+					mylogger.info("This is info level message" + e.getMessage());
+
+					BasicConfigurator.configure();
+					mylogger.debug("This is debug level message"
+							+ e.getMessage());
+
 				}
 			}
 		});
